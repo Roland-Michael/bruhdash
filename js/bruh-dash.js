91d1a3bdf464852bb47ffed95b97b4fe66dd91e9
Roland Manangan <roland.manangan@gmail.com>
2018-12-18 18:54:19 -1000

    got difference

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,43 +10,65 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (array) {
+      return array.shift();
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function (array) {
+      return array.pop();
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (array, x) {
+    return array.indexOf(x);
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (array, x) {
+    return array.lastIndexOf(x);
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (array) {
+    array.pop();
+    return array;
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
-
-  },
+  compact: function (array) {
+    var newArray = [];
+    for(var i = 0; i < array.length; i++){
+      if(array[i]){
+        newArray.push(array[i]);
+      }
+        //false, null, 0, "", undefined, and NaN
+        // array[i] !== false || array[i] !== null || array[i] !== 0 || array[i] !== "" || array[i] !== undefined || array[i] !== NaN) {
+      }
+      return newArray;
+    },
+  
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (array,x,y) {
+    var newArray = [];
+    for(var i = x; i < y; i++){
+      newArray.push(array[i]);
+    }
+    return newArray;
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
+  drop: function(array, n){
+    var newArray = [];
+    if(n === 0){
+      newArray.push(array);
+    }else{
+    for(var i = (0+n); i < array.length; i++){
+      newArray.push(array[i]);
+    }
+  }
+    return newArray;
   },
 
   // returns a slice of array with n elements dropped from the end
@@ -71,8 +93,13 @@ global.bruhdash = {
   },
 
   // removes all given values from an array
-  pull: function () {
+  pull: function (array, value) {
 
+    // PASSES BUT NOT CORRECT
+    // for(var i = 0; i < array.length; i++){
+    //   array.splice(value,1);
+    // }
+    // return array;
   },
 
   // removes elements of an array corresponding to the given indices
@@ -86,8 +113,26 @@ global.bruhdash = {
   },
 
   // returns an array with specified values excluded
-  difference: function() {
-
+  difference: function(array1, array2) {
+    var array = [];
+    array1.sort();
+    array2.sort();
+    console.log(array1);
+    console.log(array2);
+    for(var i = 0; i < array1.length; i++){
+      for(var x = 0; x <array2.length; x++){
+        if(array1[i] === array2[x]){
+          console.log(array1[i]);
+          console.log(array2[x]);
+          break;
+        }else if(array2[x] >= array1[i]){
+          array.push(array1[i]);
+          break;
+        }
+      }    
+    }
+    console.log(array);
+    return array;
   },
 
   /*******************
@@ -101,7 +146,7 @@ global.bruhdash = {
 
   // creates an array of grouped elements in their pre-zip configuration
   unzip: function () {
-
+    
   },
 
   // creates an array of elements into groups of length of specified size
